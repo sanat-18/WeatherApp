fb194746b544970b75c6a6eafad6060c85d406a9
Sanat Salian <sanat.salian@robosoftin.com>
2024-04-19 14:47:39 +0530

    Added intial test case

--- WeatherApp/WeatherApp/Controller/BaseViewController/BaseViewController.swift
+++ WeatherApp/WeatherApp/Controller/BaseViewController/BaseViewController.swift
@@ -29,6 +29,34 @@ class BaseViewController: UIViewController {
         activityIndicator.stopAnimating()
         view.isUserInteractionEnabled = true
     }
+    
+    func showAlertWithTitle(_ title: String, message: String, leftButtonTitle: String,rightButtonTitle: String, completionHandler:((_ isLeftButtonTapped: Bool) ->Void)?){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let titleFont = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18)]
+        let messageFont = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14)]
+        
+        let titleAttrString = NSMutableAttributedString(string: title, attributes: titleFont)
+        let messageAttrString = NSMutableAttributedString(string: message, attributes: messageFont)
+        
+        alert.setValue(titleAttrString, forKey: "attributedTitle")
+        alert.setValue(messageAttrString, forKey: "attributedMessage")
+        
+        let leftAction = UIAlertAction(title: leftButtonTitle, style: .default) { action in
+            if let handler = completionHandler{
+                handler(true)
+            }
+        }
+        
+        let rightAction = UIAlertAction(title: rightButtonTitle, style: .default) { action in
+            if let handler = completionHandler{
+                handler(false)
+            }
+        }
+        alert.addAction(leftAction)
+        alert.addAction(rightAction)
+        self.present(alert, animated: true, completion: nil)
+    }
 
   
 }
--- WeatherApp/WeatherAppTests/WeatherAppTests.swift
+++ WeatherApp/WeatherAppTests/WeatherAppTests.swift
@@ -8,29 +8,211 @@
 import XCTest
 @testable import WeatherApp
 
-final class WeatherAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class WeatherModelTests: XCTestCase {
+    
+    func testWeatherModelProperties() {
+        // Given
+        let cityName = "London"
+        let conditionID = 800
+        let temperature: Float = 20.5
+        let feelsLike: Float = 19.0
+        
+        // When
+        let weatherModel = WeatherModel(cityName: cityName, conditionID: conditionID, temperature: temperature, feelsLike: feelsLike)
+        
+        // Then
+        XCTAssertEqual(weatherModel.cityName, cityName)
+        XCTAssertEqual(weatherModel.conditionID, conditionID)
+        XCTAssertEqual(weatherModel.temperature, temperature)
+        XCTAssertEqual(weatherModel.feelsLike, feelsLike)
+        XCTAssertEqual(weatherModel.temperatureString, "20.5")
+        XCTAssertEqual(weatherModel.feelsLikeTemperatureString, "19.0")
+        XCTAssertEqual(weatherModel.weatherCondition, "sun.max")
     }
+}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class WeatherDataTests: XCTestCase {
+
+    func testWeatherDataDecoding() {
+        // Given
+        let json = """
+        {
+            "name": "London",
+            "main": {
+                "temp": 20.5,
+                "feelsLike": 18.0
+            },
+            "weather": [
+                {
+                    "id": 800
+                }
+            ]
+        }
+        """.data(using: .utf8)!
+        
+        // When
+        do {
+            let decoder = JSONDecoder()
+            let weatherData = try decoder.decode(WeatherData.self, from: json)
+            
+            // Then
+            XCTAssertEqual(weatherData.name, "London")
+            XCTAssertEqual(weatherData.main.temp, 20.5)
+            XCTAssertEqual(weatherData.main.feelsLike, 18.0)
+            XCTAssertEqual(weatherData.weather.count, 1)
+            XCTAssertEqual(weatherData.weather[0].id, 800)
+        } catch {
+            XCTFail("Failed to decode WeatherData: \(error)")
         }
     }
 
+    func testMainDecoding() {
+        // Given
+        let json = """
+        {
+            "temp": 20.5,
+            "feelsLike": 18.0
+        }
+        """.data(using: .utf8)!
+        
+        // When
+        do {
+            let decoder = JSONDecoder()
+            let main = try decoder.decode(Main.self, from: json)
+            
+            // Then
+            XCTAssertEqual(main.temp, 20.5)
+            XCTAssertEqual(main.feelsLike, 18.0)
+        } catch {
+            XCTFail("Failed to decode Main: \(error)")
+        }
+    }
+    
+    func testMainDecodingWithError() {
+        // Given
+        let json = """
+        {
+            "temp": 20.5,
+            "feelsLike": "18.0"
+        }
+        """.data(using: .utf8)!
+        
+        // When
+        do {
+            let decoder = JSONDecoder()
+            let main = try decoder.decode(Main.self, from: json)
+            
+            // Then
+            XCTAssertEqual(main.temp, 20.5)
+            XCTAssertEqual(main.feelsLike, 18.0)
+        } catch {
+            XCTAssertEqual(error.localizedDescription, "The data couldn’t be read because it isn’t in the correct format.")
+        }
+    }
+    
+    func testWeatherDecoding() {
+        // Given
+        let json = """
+        {
+            "id": 800
+        }
+        """.data(using: .utf8)!
+        
+        // When
+        do {
+            let decoder = JSONDecoder()
+            let weather = try decoder.decode(Weather.self, from: json)
+            
+            // Then
+            XCTAssertEqual(weather.id, 800)
+        } catch {
+            XCTFail("Failed to decode Weather: \(error)")
+        }
+    }
+}
+
+
+class WeatherManagerTests: XCTestCase {
+    
+    class MockDelegate: WeatherManagerDelegate {
+        var updateWeatherCalled = false
+        var error: Error?
+        var weather: WeatherModel?
+        
+        func didUpdateWeatherManager(_ weather: WeatherModel) {
+            updateWeatherCalled = true
+            self.weather = weather
+        }
+        
+        func didFailWithError(error: Error) {
+            self.error = error
+        }
+    }
+    
+    func testFetchCityWeather() {
+        // Given
+        let manager = WeatherManager.sharedManager
+        let mockDelegate = MockDelegate()
+        manager.delegate = mockDelegate
+        let cityName = "London"
+        
+        // When
+        manager.fetchCityWeather(cityName)
+        
+        // Then
+        XCTAssertNotNil(manager.delegate)
+        XCTAssertNotNil(manager.delegate === mockDelegate)
+    }
+    
+    func testFetchCurrentWeather() {
+        // Given
+        let manager = WeatherManager.sharedManager
+        let mockDelegate = MockDelegate()
+        manager.delegate = mockDelegate
+        let latitude: Float = 50.5074
+        let longitude: Float = 13.1278
+        
+        // When
+        manager.fetchCurrentWeather(latitude: latitude, longitude: longitude)
+        
+        // Then
+        XCTAssertNotNil(manager.delegate)
+        XCTAssertNotNil(manager.delegate === mockDelegate)
+    }
+    
+    
+    func testParseJson() {
+        // Given
+        let manager = WeatherManager.sharedManager
+        let mockDelegate = MockDelegate()
+        manager.delegate = mockDelegate
+        
+        // Sample JSON data
+        let json = """
+        {
+            "name": "London",
+            "main": {
+                "temp": 20.5,
+                "feelsLike": 18.0
+            },
+            "weather": [
+                {
+                    "id": 800
+                }
+            ]
+        }
+        """.data(using: .utf8)!
+        
+        // When
+        let weatherModel = manager.parseJson(json)
+        
+        // Then
+        XCTAssertNotNil(weatherModel)
+        XCTAssertEqual(weatherModel?.cityName, "London")
+        XCTAssertEqual(weatherModel?.conditionID, 800)
+        XCTAssertEqual(weatherModel?.temperature, 20.5)
+        XCTAssertEqual(weatherModel?.feelsLike, 18.0)
+    }
 }
